05eeab1aca0f35c63e0b7b0480bbc2ed52aa195a
siyou <kchaoes@email.com>
2022-02-07 01:31:41 +0800

    refactor machine and node

--- biz/src/lib.rs
+++ biz/src/lib.rs
@@ -1,24 +1,9 @@
-use std::{
-    collections::HashMap,
-    io::{self, BufRead},
-    sync::{Arc, Mutex},
-};
+use std::io::{self, BufRead};
 
-use frame_client::LaunchInfo;
-use frame_common::{
-    data::{BridgeMessage, Router},
-    get_rsa, get_runtime, sign,
-};
-use custom::{
-    base::{
-        node::register_node,
-    },
-    get_relayer, send_msg, relayer::Relayer, machine::{register_custom_tasks, get_client_regiser, CustomTaskInfo},
-};
+use custom::{get_custom, register_node};
+use frame_common::get_runtime;
 
-use rsa::RsaPrivateKey;
-use threadpool::{Builder, ThreadPool};
-use tokio::{runtime::Runtime, sync::mpsc::Sender};
+use tokio::runtime::Runtime;
 
 mod tests;
 
@@ -42,24 +27,9 @@ pub fn launch_service() {
     let stdin = io::stdin();
     let mut handle = stdin.lock();
 
-    // if relayer can't launch,panic
-    let relayer = get_relayer().unwrap();
-    let custom_task_register = register_custom_tasks();
-    let client_register = get_client_regiser();
-    let mut input_map: HashMap<String, Sender<BridgeMessage>> = HashMap::new();
-    // name->group
-    let mut group_map: HashMap<String, String> = HashMap::new();
-    let mut priv_keys: HashMap<String, RsaPrivateKey> = HashMap::new();
-
-    //custom thread pool
-    let pool = Arc::new(Mutex::new(
-        Builder::new()
-            .num_threads(2)
-            .thread_name(String::from("threadpool"))
-            .build(),
-    ));
-
     let rt = get_runtime();
+
+    let (mut machine, relayer) = get_custom().unwrap();
 
     let mut flag: bool = false;
     loop {
@@ -79,32 +49,13 @@ pub fn launch_service() {
         let command = result.unwrap();
         match command {
             Command::AddClient { name, group, addr } => {
-                if let Err(error) = add_client(
-                    &rt,
-                    name,
-                    group,
-                    addr,
-                    &mut group_map,
-                    pool.clone(),
-                    custom_task_register.clone(),
-                    &relayer,
-                    client_register.clone(),
-                    &mut input_map,
-                    &mut priv_keys,
-                ) {
+                if let Err(error) = register_node(&rt, &name, &group, &addr, &mut machine, &relayer)
+                {
                     println!("add client failed,error={}", error);
-                };
+                }
             }
             Command::SendMsg { from, to, content } => {
-                if let Err(error) = send_message(
-                    &rt,
-                    from,
-                    to,
-                    content,
-                    &mut input_map,
-                    &mut group_map,
-                    &mut priv_keys,
-                ) {
+                if let Err(error) = &machine.send_message(&rt, from.clone(), to.clone(), content) {
                     println!("send msg failed,error={}", error);
                 }
             }
@@ -117,92 +68,18 @@ pub fn launch_service() {
 
     if (flag) {
         drop(relayer);
-        drop(custom_task_register);
-        drop(input_map);
-        drop(client_register);
+        drop(machine);
+        println!("do other customize things like drop or other tasks");
     }
-}
-
-fn add_client(
-    rt: &Runtime,
-    name: Box<String>,
-    group: Box<String>,
-    addr: Box<String>,
-    group_map: &mut HashMap<String, String>,
-    pool: Arc<Mutex<ThreadPool>>,
-    custom_task_register: Sender<CustomTaskInfo>,
-    relayer: &Relayer<BridgeMessage>,
-    client_register: Sender<LaunchInfo<BridgeMessage>>,
-    input_map: &mut HashMap<String, Sender<BridgeMessage>>,
-    priv_keys: &mut HashMap<String, RsaPrivateKey>,
-) -> Result<(), String> {
-    check_fresh_client(&group_map, &name)?;
-    let (pri_key, pub_key) = get_rsa()?;
-
-    register_node(
-        rt,
-        &name,
-        &group,
-        &addr,
-        client_register.clone(),
-        relayer,
-        custom_task_register.clone(),
-        pool,
-        pub_key,
-    )
-    .map(|input| input_map.insert(*name.clone(), input))?;
-
-    priv_keys.insert(*name.clone(), pri_key);
-    group_map.insert(*name.clone(), *group);
-
-    Ok(())
-}
-
-fn send_message(
-    rt: &Runtime,
-    from: Box<String>,
-    to: Box<String>,
-    content: Box<String>,
-    input_map: &HashMap<String, Sender<BridgeMessage>>,
-    group_map: &HashMap<String, String>,
-    priv_keys: &HashMap<String, RsaPrivateKey>,
-) -> Result<(), String> {
-    let name = from.clone().to_string();
-    let to_name = to.clone().to_string();
-    let sender = input_map.get(&name).ok_or("sender do not exist or init!")?;
-    let from_group = group_map
-        .get(&name)
-        .ok_or("sender group do not exist or init!")?;
-    let to_group = group_map
-        .get(&to_name)
-        .ok_or("receiver do not exist or init!")?;
-    let priv_key = priv_keys.get(&name).ok_or("private key do not exist!")?;
-
-    let mut bridge_message = BridgeMessage {
-        from_name: Box::new(name),
-        from_group: Box::new(from_group.to_string()),
-        to_name: Box::new(to_name),
-        to_group: Box::new(to_group.to_string()),
-        message: content,
-        error_msg: None,
-        sig: None,
-    };
-
-    let sig = sign(&bridge_message.get_source_id(), priv_key)?;
-    bridge_message.sig = Some(sig);
-
-    send_msg(sender, &rt, bridge_message);
-    Ok(())
-}
-
-fn check_fresh_client(map: &HashMap<String, String>, name: &str) -> Result<(), String> {
-    if map.contains_key(name) {
-        return Err("client already exitst".to_string());
-    }
-    Ok(())
 }
 
 fn parse_command(input: &str) -> Result<Command, &str> {
+    match input.trim() {
+        "Shutdown" => {
+            return Ok(Command::Shutdown);
+        }
+        _ => (),
+    }
     let inputs: Vec<&str> = input.trim().split('{').collect();
     if inputs.len() != 2 {
         return Err("Error! input style should be 'Command{{xxx}}'");
